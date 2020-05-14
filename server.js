44d868e1a4d92ebafa1a8c34946c6aefc5ef321b
Ahmed Chiaeb <ahmed.chaieb@epitech.eu>
2020-05-14 03:38:26 +0200

    d

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,8 +17,34 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-;
+const botName = 'ChatCord Bot';
 
+
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+ 
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+  
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+  
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  
 
 
 const PORT = process.env.PORT || 3000;
